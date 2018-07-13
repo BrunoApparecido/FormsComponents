fc245ca4713bb7752c2369fb567bed37686c572b
Bruno Alexandre <brunoapparecido@gmail.com>
2018-07-13 11:53:48 -0300

    - Commit Inicial

---
+++ FormsComponents/FormsComponents/ListView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FormsComponents
+{
+    public class ListView : Xamarin.Forms.ListView
+    {
+        public static readonly BindableProperty ItemTappedCommandProperty =
+          BindableProperty.Create(nameof(ItemTappedCommand),
+                            typeof(ICommand),
+                            typeof(ListView),
+                            null);
+        public ICommand ItemTappedCommand
+        {
+            get { return (ICommand)GetValue(ItemTappedCommandProperty); }
+            set { SetValue(ItemTappedCommandProperty, value); }
+        }
+
+        public static readonly BindableProperty InfiniteScrollCommandProperty =
+            BindableProperty.Create(nameof(InfiniteScrollCommand),
+                    typeof(ICommand),
+                    typeof(ListView),
+                    null);
+        public ICommand InfiniteScrollCommand
+        {
+            get { return (ICommand)GetValue(InfiniteScrollCommandProperty); }
+            set { SetValue(InfiniteScrollCommandProperty, value); }
+        }
+
+        public ListView()
+            : base()
+        {
+            Initialize();
+        }
+        public ListView(ListViewCachingStrategy cachingStrategy)
+            : base(cachingStrategy)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            ItemAppearing += InfiniteListView_ItemAppearing;
+            ItemTapped += ListView_ItemTapped;
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (ItemTappedCommand != null && ItemTappedCommand.CanExecute(null))
+                ItemTappedCommand.Execute(e.Item);
+        }
+
+        private void InfiniteListView_ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            var items = ItemsSource as IList;
+            if (items != null && e.Item == items[items.Count - 3])
+            {
+                if (InfiniteScrollCommand != null && InfiniteScrollCommand.CanExecute(null))
+                    InfiniteScrollCommand.Execute(null);
+            }
+        }
+    }
+}
---
+++ FormsComponents/FormsComponents/SearchBar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FormsComponents
+{
+    public class SearchBar : Xamarin.Forms.SearchBar
+    {
+        public static readonly BindableProperty TextChangedCommandProperty =
+          BindableProperty.Create(nameof(TextChangedCommand),
+                            typeof(ICommand),
+                            typeof(SearchBar),
+                            null);
+
+        public ICommand TextChangedCommand
+        {
+            get { return (ICommand)GetValue(TextChangedCommandProperty); }
+            set { SetValue(TextChangedCommandProperty, value); }
+        }
+
+        public static readonly BindableProperty AutoSearchdProperty =
+          BindableProperty.Create(nameof(AutoSearch),
+                            typeof(bool),
+                            typeof(SearchBar),false);
+
+        public bool AutoSearch
+        {
+            get { return (bool)GetValue(AutoSearchdProperty); }
+            set { SetValue(AutoSearchdProperty, value); }
+        }
+
+        public SearchBar()
+        {
+            TextChanged += (e, a) =>
+            {
+                if (TextChangedCommand != null && TextChangedCommand.CanExecute(null))
+                    TextChangedCommand.Execute(null);
+            };
+
+            TextChanged += (e, a) =>
+            {
+                if (AutoSearch)
+                {
+                    if (SearchCommand != null && SearchCommand.CanExecute(null))
+                        SearchCommand.Execute(null);
+
+                }
+            };
+        }
+    }
+}
